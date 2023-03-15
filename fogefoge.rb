e259315cfa1f009890b3596d57efdfbf37ced2ea
Joao Guilherme <joomoreira36@gmail.com>
2023-03-14 22:12:56 -0300

    adicionando a funcionalidade de bomba no jogo

--- fogefoge.rb
+++ fogefoge.rb
@@ -86,7 +86,7 @@ def jogador_perdeu?(mapa)
 end
 
 def joga(nome)
-  mapa = le_mapa 2
+  mapa = le_mapa 3
   while true
     desenha mapa
     direcao = pede_movimento
@@ -96,6 +96,11 @@ def joga(nome)
       next
     end
     heroi.remove_do mapa
+    if mapa[nova_posicao.linha][nova_posicao.coluna] =="*"
+      for direita in 1..4
+        mapa[nova_posicao.linha][nova_posicao.coluna + direita] = " "
+      end
+    end
     nova_posicao.coloca_no mapa
 
     mapa = move_fantasmas mapa
